574eac53cfee429a4d7c77e7b569d0fad25503ba
George Francis <georgedoescode@gmail.com>
2022-07-05 15:29:38 +0100

    Add cache to gitignonre

--- plugins/blog-cache/index.js
+++ plugins/blog-cache/index.js
@@ -5,13 +5,17 @@ module.exports = {
   //  - the directory already exists locally
   //  - the directory has never been cached
   async onPreBuild({ utils }) {
-    await utils.cache.restore("./_blog-cache");
+    await utils.cache.restore("./_blog-cache", {
+      cwd: "/",
+    });
   },
   // After the build is done,
   // cache directory for future builds.
   // Does not do anything if:
   //  - the directory does not exist
   async onPostBuild({ utils }) {
-    await utils.cache.save("./_blog-cache");
+    await utils.cache.save("./_blog-cache", {
+      cwd: "/",
+    });
   },
 };
