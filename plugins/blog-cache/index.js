8dea7e3fe26a026dcbf9a6c37c466aca180677cf
George Francis <georgedoescode@gmail.com>
2022-07-05 15:32:14 +0100

    Update cache dir

--- plugins/blog-cache/index.js
+++ plugins/blog-cache/index.js
@@ -5,17 +5,13 @@ module.exports = {
   //  - the directory already exists locally
   //  - the directory has never been cached
   async onPreBuild({ utils }) {
-    await utils.cache.restore("./_blog-cache", {
-      cwd: "/",
-    });
+    await utils.cache.restore("./_blog-cache");
   },
   // After the build is done,
   // cache directory for future builds.
   // Does not do anything if:
   //  - the directory does not exist
   async onPostBuild({ utils }) {
-    await utils.cache.save("./_blog-cache", {
-      cwd: "/",
-    });
+    await utils.cache.save("./_blog-cache");
   },
 };
