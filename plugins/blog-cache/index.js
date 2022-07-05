e1f60189a3e6a2839b5e40cee2df480f43503f03
George Francis <georgedoescode@gmail.com>
2022-07-05 16:53:27 +0100

    Check which files need to be rebuiltt

--- plugins/blog-cache/index.js
+++ plugins/blog-cache/index.js
@@ -5,7 +5,10 @@ module.exports = {
   async onPreBuild({ utils, git }) {
     // TODO - can we use modifiedFiles here to determine wether we should do a full rebuild?
     console.log(utils.git.modifiedFiles);
-    await utils.cache.restore("./_blog-cache");
+
+    if (!utils.git.modifiedFiles.contains("blog-post.vue")) {
+      await utils.cache.restore("./_blog-cache");
+    }
   },
 
   async onPostBuild({ utils }) {
