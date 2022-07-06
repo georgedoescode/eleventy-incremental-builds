090981250096051b4bc7b7bee7a6130c749de0a6
George Francis <georgedoescode@gmail.com>
2022-07-06 11:02:33 +0100

    Add lots of blogs

--- blog-post.vue
+++ blog-post.vue
@@ -29,7 +29,7 @@ export default {
         alias: "post",
       },
       permalink: (data) => `blog/${data.post.slug}/index.html`,
-      bigNumber: fibonacci.iterate(4000),
+      bigNumber: fibonacci.iterate(100),
     };
   },
 };
