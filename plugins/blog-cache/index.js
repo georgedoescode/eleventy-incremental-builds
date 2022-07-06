7eeeaee23f7b24a875fa8d4abad55a83b40d81f1
George Francis <georgedoescode@gmail.com>
2022-07-06 10:34:24 +0100

    Add a check to make sure all posts have been written

--- plugins/blog-cache/index.js
+++ plugins/blog-cache/index.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const fse = require("fs-extra");
+const { getPosts } = require("../../mock-blog-api");
 
 module.exports = {
   async onPreBuild({ utils, git }) {
@@ -15,5 +16,15 @@ module.exports = {
     fse.copySync("./_site/blog", "./_blog-cache/posts");
 
     await utils.cache.save("./_blog-cache");
+
+    // Ensure all the posts exist, otherwise fail the build
+    const allSiteSlugs = fs.readdirSync("./_site/blog").map((s) => `/${s}/`);
+    const allSlugs = getPosts().posts.map((post) => post.slug);
+
+    const allPostsWritten = allSlugs.every((s) => allSiteSlugs.includes(s));
+
+    if (!allPostsWritten) {
+      utils.build.failBuild("BUIlD FAILURE - SOME BLOG POSTS FAILED TO BUILD");
+    }
   },
 };
