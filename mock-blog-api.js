6b9989608f773a8ba61e55755d50742cb33aba8c
George Francis <georgedoescode@gmail.com>
2022-07-06 10:58:04 +0100

    Add blog post

--- mock-blog-api.js
+++ mock-blog-api.js
@@ -1,4 +1,11 @@
-const posts = [];
+const posts = [
+  {
+    _id: "0",
+    title: "Blog Post 1",
+    slug: "/blog-post-1/",
+    lastUpdated: "2021-01-07",
+  },
+];
 
 function getPosts() {
   return {
