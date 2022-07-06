067976134dd4306009e2c0de7ad9036096edbecf
George Francis <georgedoescode@gmail.com>
2022-07-06 11:53:10 +0100

    Update posts

--- mock-blog-api.js
+++ mock-blog-api.js
@@ -1,11 +1,20 @@
-const posts = [...Array(1024)].map((p, index) => {
-  return {
-    _id: index,
-    title: `Blog Post ${index}`,
-    slug: `/blog-post-${index}/`,
-    lastUpdated: "2022-01-01",
-  };
-});
+const posts = [
+  {
+    _id: "0",
+    title: "Blog Post 1",
+    slug: "/blog-post-1/",
+    lastUpdated: "2021-01-08",
+  },
+];
+
+// const posts = [...Array(1024)].map((p, index) => {
+//   return {
+//     _id: index,
+//     title: `Blog Post ${index}`,
+//     slug: `/blog-post-${index}/`,
+//     lastUpdated: "2022-01-01"
+//   }
+// })
 
 function getPosts() {
   return {
