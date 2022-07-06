23a9eddb83f2b932ca4f5ed7cef3bd502d70c720
George Francis <georgedoescode@gmail.com>
2022-07-06 17:57:50 +0100

    Update homepage

--- index.vue
+++ index.vue
@@ -15,6 +15,13 @@
           debitis laboriosam praesentium commodi neque consequatur beatae nulla
           officiis nisi dolores. Ipsam, impedit. Possimus, delectus.
         </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum quia
+          minus nesciunt aut sunt, necessitatibus odio fuga velit veritatis
+          commodi porro, quas magni, incidunt iure recusandae delectus
+          praesentium est nulla. At debitis, possimus temporibus aliquid tenetur
+          dignissimos quos! Amet, molestiae.
+        </p>
       </main>
     </body>
   </html>
